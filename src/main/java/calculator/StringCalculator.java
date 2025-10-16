22b6acd132fefe17ab3009fed4e8bada525d3635
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2025-10-17 08:16:51 +0900

    feat(validator): add integer validation for tokens and handle non-numeric or negative inputs with IllegalArgumentException

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public static int add(String input){
         // 1. null, 빈 문자열, 공백만 있을 경우 -> 0 반환
@@ -7,27 +9,53 @@ public class StringCalculator {
             return 0;
         }
 
-        String numbers = input; // 실제 숫자 부분만
+        // 콘솔에서 입력한 "\n" 리터럴을 실제 개행으로 치환
+        input = input.replace("\\n", "\n");
+
+        String numbers = input;
         String delimiter = "[,:]";
 
         // 2. 커스텀 구분자로 여부 확인
         if(input.startsWith("//")){
             int newLineIndex = input.indexOf("\n"); // \n 위치 찾기
-            delimiter = input.substring(2, newLineIndex); // //와 /n 사이 문자 추출
+            if (newLineIndex < 0 || newLineIndex == 2) {
+                throw new IllegalArgumentException("Invalid custom delimiter header");
+            }
+            String custom = input.substring(2, newLineIndex); // //와 /n 사이 문자 추출
             numbers = input.substring(newLineIndex + 1);
+            delimiter = Pattern.quote(custom);
 
-            // 정규식 특수문자를 구분자로 사용할 떄 안전하게 정리.
-            delimiter = java.util.regex.Pattern.quote(delimiter);
+
         }
 
         //3. 분리
         String[] tokens = numbers.split(delimiter);
 
-        //4. 합산
+        //4. 각 토큰 정수 여부 검사 + 변환 + 합산
         int sum = 0;
         for (String token : tokens){
             String t = token.trim();
-            sum += Integer.parseInt(t);
+
+            // check1 빈 토큰 검사
+            if (t.isEmpty()){
+                throw new IllegalArgumentException("Empty number token is not allowed.");
+            }
+
+            // check2 정수 형식 검사
+            // [0-9]+ → 숫자 1개 이상인지 확인
+            if (!t.matches("-?[0-9]+")) {  // (음수 검사도 포함)
+                throw new IllegalArgumentException("Non-numeric token: " + t);
+            }
+
+            // check3 정수 변환
+            int num = Integer.parseInt(t);
+
+            // check4 음수 검사
+            if (num < 0) {
+                throw new IllegalArgumentException("Negative number not allowed: " + num);
+            }
+
+            sum += num;
         }
 
         return sum;
